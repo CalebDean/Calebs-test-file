59160445b5e6a0bca015f5433e90e51d92b3cdb2
CalebDean <Caleb.dean@hotmail.co.uk>
2015-11-24 09:09:33 +0000

    first commit

---
+++ Calebs FIrst Web App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Calebs_FIrst_Web_App.Startup))]
+namespace Calebs_FIrst_Web_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
